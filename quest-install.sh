1713d74043d6c4795e1edf16d24f2fa8a39af403
AndKe <andre.kjellstrup@gmail.com>
2023-11-22 20:24:32 +0100

    Rework to handle apps w/o obb

--- quest-install.sh
+++ quest-install.sh
@@ -24,31 +24,33 @@ if [ ! -d "$dir" ]; then
 fi
 
 # Check if the directory contains exactly one other directory and one .apk file
-dir_count=$(find "$dir" -mindepth 1 -maxdepth 1 -type d | wc -l)
 apk_count=$(find "$dir" -mindepth 1 -maxdepth 1 -type f -name "*.apk" | wc -l)
-
-if [ $dir_count -ne 1 ] || [ $apk_count -ne 1 ]; then
-    echo "Error: $dir does not contain exactly one directory and one .apk file"
+if [ $apk_count -ne 1 ]; then
+    echo "Error: $dir does not contain exactly one .apk file"
     echo "Contents of $dir:"
     ls "$dir"
     exit 1
 fi
+apk_file=$(find "$dir" -mindepth 1 -maxdepth 1 -type f -name "*.apk")
 
 # Get the full path of the directory and the apk file
 subdir=$(find "$dir" -mindepth 1 -maxdepth 1 -type d)
-apk_file=$(find "$dir" -mindepth 1 -maxdepth 1 -type f -name "*.apk")
+dir_count=$(find "$dir" -mindepth 1 -maxdepth 1 -type d | wc -l)
+if [ $dir_count -ne 1 ]; then
+    echo "Found no obb directory, skipping obb copy"
+else
+    #make directory first needed for Quest3
+    echo "-Making obb directory, if exists an error will appear, that is ok."
+    adb shell mkdir $(basename "$subdir") /sdcard/Android/obb/
 
-#make directory first needed for Quest3
-echo "-Making obb directory, if exists an error will appear, that is ok."
-adb shell mkdir $(basename "$subdir") /sdcard/Android/obb/
+    #Copy obb
+    echo "-Copying obb directory"
+    adb push "$subdir" /sdcard/Android/obb/
+fi
 
-#Copy obb
-echo "-Copying obb directory"
-adb push "$subdir" /sdcard/Android/obb/
 
 #install apk
 echo "-Installing .apk"
 adb install -g -r "$apk_file"
 
-echo "Done"
-
+echo "-Done"
